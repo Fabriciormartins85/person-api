665200fcbd23c3a2d9cb99a32f4c4735b9b73107
Fabrício Martins <fabriciormartins85@gmail.com>
2021-10-25 14:29:17 -0300

    Add service layer for business handling

--- src/main/java/br/com/example/domain/service/PersonService.java
+++ src/main/java/br/com/example/domain/service/PersonService.java
@@ -1,5 +1,35 @@
 package br.com.example.domain.service;
 
+import java.util.Optional;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.example.api.exception.NotFoundException;
+import br.com.example.domain.dto.PersonDTO;
+import br.com.example.domain.model.Person;
+import br.com.example.domain.repositorie.PersonRepository;
+
+@Service
 public class PersonService {
+
+	private PersonRepository repo;
+
+	private ModelMapper mapper;
+	
+	@Autowired
+	public PersonService(PersonRepository repo, ModelMapper mapper) {
+		this.repo = repo;
+		this.mapper = mapper;
+	}
+
+	public PersonDTO findById(final Long id) throws NotFoundException {
+		Optional<Person> optPerson = repo.findById(id);
+		return mapper.map(optPerson.orElseThrow(() -> new NotFoundException("Person Not Found")), PersonDTO.class);
+	}
+	public PersonDTO save(PersonDTO personDTO) throws NotFoundException {
+		return mapper.map(repo.save(mapper.map(personDTO, Person.class)), PersonDTO.class);
+	}
 
 }
