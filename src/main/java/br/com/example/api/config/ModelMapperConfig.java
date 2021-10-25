3976d743d56c59c7095734ddd3e4ec0d2198d12f
Fabrício Martins <fabriciormartins85@gmail.com>
2021-10-25 14:19:53 -0300

    Included beans validations and ModelMapper to convert entities to dto

---
+++ src/main/java/br/com/example/api/config/ModelMapperConfig.java
@@ -0,0 +1,23 @@
+package br.com.example.api.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
+
+@Configuration
+public class ModelMapperConfig {
+
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
+
+}
+@Configuration
+class PersistenceHibernateConfig{
+	   @Bean
+	   public PersistenceExceptionTranslationPostProcessor exceptionTranslation(){
+	      return new PersistenceExceptionTranslationPostProcessor();
+	   }
+	}
